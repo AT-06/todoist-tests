e8384f9751ae3eeddffcb2917a3b3c05c49bdb1b
Omar Limbert Huanca Sanchez <omar@programadorez.com>
2018-05-11 06:08:21 -0400

    beforeach added
    beforeach added

--- specs/project.spec.js
+++ specs/project.spec.js
@@ -1,7 +1,4 @@
 // project.spec.js
-const projectToAddName = 'Project to Added';
-const projectToModify = 'Project MODIFIED';
-const projectToDelete = 'Project to delete';
 let config = require('../config.json');
 let expect = require("chai").expect;
 let LoginPage = require('../pages/LoginPage'); // update this
@@ -9,32 +6,28 @@ let ProjectPage = require('../pages/ProjectPage');
 
 
 describe('Acceptance Tests to Project feature', function () {
-    it('should allow to add new project', function () {
+    beforeEach(function () {
         // Login on website with credentials.
-        LoginPage.login(config.email, config.password);
-        // Adding new project with name "projectToAddName".
-        ProjectPage.addProject(projectToAddName);
+        LoginPage.login(config.acc1_email, config.acc1_password);
+    });
+
+    it('should allow to add new project', function () {
+        ProjectPage.addProject(config.projectToAdd);
         // Verify if last Project added name is equal to "projectToAddName".
-        expect(ProjectPage.lastProjectOnList.getText()).to.have.equal(projectToAddName);
+        expect(ProjectPage.lastProjectOnList.getText()).to.have.equal(config.projectToAdd);
         // Verify if last project name as shown on editor.
-        expect(ProjectPage.assertOnEditor).to.have.contain(projectToAddName)
+        expect(ProjectPage.assertOnEditor).to.have.contain(config.projectToAdd)
     });
 
     it('should allow to Modify a project', function () {
-        // Login on website with credentials.
-        LoginPage.login(config.email, config.password);
-        // Adding and Modifying one Project.
-        ProjectPage.modifyProject(projectToAddName, projectToModify);
+        ProjectPage.modifyProject(config.projectToAdd, config.projectToModify);
         // Verify if last project added "Project to Added" has been changed to "Project MODIFIED".
-        expect(ProjectPage.lastProjectOnList.getText()).to.have.equal(projectToModify)
+        expect(ProjectPage.lastProjectOnList.getText()).to.have.equal(config.projectToModify)
     });
 
     it('should allow to delete a project', function () {
-        // Login on website with credentials.
-        LoginPage.login(config.email, config.password);
-        // Adding and Deleting one Project".
-        ProjectPage.deleteProject(projectToDelete);
+        ProjectPage.deleteProject(config.projectToDelete);
         // Verify if last project added "Project to delete" has been deleted.
-        expect(ProjectPage.lastProjectOnList.getText()).to.have.not.equal(projectToDelete);
+        expect(ProjectPage.lastProjectOnList.getText()).to.have.not.equal(config.projectToDelete);
     });
 });
