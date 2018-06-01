22d5ecfe9491959876517079bb53f95b0a58031a
manu863018 <manuel.valdez@fundacion-jala.org>
2018-06-01 23:18:40 +0000

    Add get, post, put and delete methods for request manager.

--- api/RequestManager.js
+++ api/RequestManager.js
@@ -1,25 +1,72 @@
 let axios = require('axios');
+let config = require('../config.json');
 
 class RequestManager {
+    constructor() {
+        this.status;
+        this.error;
+    }
     intanceRequests() {
         return axios.create({
-            baseURL: 'https://beta.todoist.com/API/v8',
-            timeout: 5000,
+            baseURL: config.api_URL,
+            timeout: 10000,
             headers: {
-                'Authorization': 'Bearer ea8c82aa2deaed6de59d372d0e25ac05e09697c2'
+                'Authorization': config.api_Token
             }
         });
     }
 
+    get(endpoint) {
+        return this.intanceRequests().request({
+            method: 'GET',
+            url: endpoint,
+            responseType: 'json'
+        }).then(response => this.setResponse(response))
+            .catch(error => this.setError(error));
+    }
+
+    post(endpoint, data) {
+        return this.intanceRequests().request({
+            method: 'POST',
+            url: endpoint,
+            responseType: 'application/json',
+            data: {
+                name: 'Project created API'
+            }
+        }).then(response => this.setResponse(response))
+            .catch(error => this.setError(error));
+    }
+
+    put(endpoint, body) {
+        return this.intanceRequests().request({
+            method: 'PUT',
+            url: endpoint,
+            responseType: 'json',
+            data: body
+        }).then(response => this.setResponse(response))
+            .catch(error => this.setError(error));
+    }
+
+    delete(endpoint) {
+        return this.intanceRequests().request({
+            method: 'DELETE',
+            url: endpoint,
+            responseType: 'json'
+        }).then(response => this.setResponse(response))
+            .catch(error => this.setError(error));
+    }
+
+    setResponse(response) {
+        console.log("status:" + response.status);
+        this.status = response.status;
+    }
+
+    setError(error) {
+        this.error = error.status;
+    }
+
     getResponse() {
-        this.intanceRequests()
-            .get('/projects')
-            .then(function(response) {
-                console.log(response);
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
+        return this.status;
     }
 }
 
--- specs/Projects/addAPIProject.spec.js
+++ specs/Projects/addAPIProject.spec.js
@@ -3,23 +3,30 @@ let expect = require('chai').expect;
 let requestManager = require('../../api/RequestManager');
 let axios = require('axios');
 
-describe('Acceptance Tests for API, post a new project', function () {
-    it('should allow to delete a project', function () {
-        requestManager.getResponse();
-        /*let instance =  axios.create({
-            baseURL: 'https://beta.todoist.com/API/v8',
-            headers: {
-                'Authorization': 'Bearer ea8c82aa2deaed6de59d372d0e25ac05e09697c2'
-            }
-        });
-        instance
-            .get('/projects')
-            .then(function(response) {
-                console.log(response);
-            })
-            .catch(function(error) {
-                console.log(error);
-            });*/
-    });
+describe('Acceptance Tests for API, get all projects', function () {
+    it('should allow to get all project', function () {
+        return requestManager.get('/projects');
 
+    });
+});
+
+describe('Acceptance Tests for API, post a new project', function () {
+    it('should allow to create a new project', function* () {
+        let data = {
+            name: 'Project created API'
+        };
+        return requestManager.post('/projects', data);
+    });
+});
+
+describe('Acceptance Tests for API, put an existing project', function () {
+    it('should allow to edit an existing project', function () {
+        return requestManager.put('/projects/id', {"name": "Project edited"});
+    });
+});
+
+describe('Acceptance Tests for API, delete a project', function () {
+    it('should allow to edit an existing project', function () {
+        return requestManager.delete('/projects/id');
+    });
 });
